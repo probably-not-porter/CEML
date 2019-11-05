44b40078a0e4f8891948b623782feb58526cbcd7
probably-not-porter <pelibby16@earlham.edu>
2019-11-05 00:23:58 -0500

    do some testing on caney

--- operations/edge_detectors.py
+++ operations/edge_detectors.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 def caney_edge(image, outdir):
     im = cv2.imread(image)
-    edges = cv2.Canny(im,50,100,L2gradient=False)
+    edges = cv2.Canny(im,50,100,L2gradient=True)
     plt.imshow(edges,cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
@@ -22,9 +22,9 @@ def caney_edge(image, outdir):
     ax.imshow(edges)
     fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=400)
 
-def caney_edge_test(image, outdir):
+def caney_edge_test(image, outdir,x):
     im = cv2.imread(image)
-    edges = cv2.Canny(im,50,100,L2gradient=False)
+    edges = cv2.Canny(im,90,110,100,L2gradient=False)
     plt.imshow(edges,cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
@@ -37,4 +37,8 @@ def caney_edge_test(image, outdir):
         ax.get_lines()[0].set_color("black")
     fig.add_axes(ax)
     ax.imshow(edges)
-    fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=400)
+    fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=1000)
+
+for x in range(1):
+    print(x)
+    caney_edge_test("/home/porter/Dev/SRSD/images/DJI_0018.JPG", "/home/porter/Dev/SRSD/test_images/test" + str(x),x)
