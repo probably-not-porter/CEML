6e22222369089e5b8d28592a1a65777f006bb08a
probably-not-porter <pelibby16@earlham.edu>
2019-11-07 13:01:32 -0500

    caney test

--- operations/edge_detectors.py
+++ operations/edge_detectors.py
@@ -5,6 +5,8 @@ from scipy import ndimage as ndi
 import cv2
 import matplotlib.pyplot as plt
 
+import time
+
 def caney_edge(image, outdir):
     my_dpi = 1000
     im = cv2.imread(image)
@@ -23,15 +25,18 @@ def caney_edge(image, outdir):
     ax.imshow(edges)
     fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=400)
 
-def caney_edge_test(image, outdir,x):
-    my_dpi = 1000
+
+
+def caney_edge_test(image, outdir): # testing version of caney function, times execution.
+    start = time.time()
+    myres = 1000
     im = cv2.imread(image)
     height, width = im.shape[:2]
     edges = cv2.Canny(im,90,110,100,L2gradient=False)
     plt.imshow(edges,cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-    fig = plt.figure(frameon=False,figsize=(width // my_dpi, height // my_dpi), dpi=300)
+    fig = plt.figure(frameon=False,figsize=(4,3))
 
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
@@ -39,8 +44,11 @@ def caney_edge_test(image, outdir,x):
         ax.get_lines()[0].set_color("black")
     fig.add_axes(ax)
     ax.imshow(edges)
-    fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=1000)
+    fig.savefig(outdir,facecolor='w', edgecolor='w', dpi=myres)
+    end = time.time()
+    print("Total time: " + end - start)
 
-#for x in range(1):
-#    print(x)
-#    caney_edge_test("/home/porter/Dev/SRSD/images/DJI_0018.JPG", "/home/porter/Dev/SRSD/test_images/test" + str(x),x)
+
+
+
+caney_edge_test("/home/porter/Dev/SRSD/images/DJI_0018.JPG", "/home/porter/Dev/SRSD/test_images/test")
