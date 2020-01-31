a6c4c91d5700a759aa196b155176260af91da097
probably-not-porter <pelibby16@earlham.edu>
2020-01-31 09:24:16 -0500

    get rid of matplot inefficiency
    Former-commit-id: b41b2bb8477fac32f4d38431fbed36b1cbc6983f

--- fast_test.py
+++ fast_test.py
@@ -1,5 +1,25 @@
+# example from 
+# https://github.com/fastai/fastai/blob/master/examples/dogs_cats.ipynb
+
 from fastai.vision import *
-path = untar_data(MNIST_PATH)
-data = image_data_from_folder(path)
-learn = cnn_learner(data, models.resnet18, metrics=accuracy)
-learn.fit(1)
+
+path = untar_data(URLs.DOGS)
+
+data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+data.show_batch(rows=4)
+
+learn = cnn_learner(data, models.resnet34, metrics=accuracy)
+learn.fit_one_cycle(1)
+
+learn.unfreeze()
+learn.fit_one_cycle(6, slice(1e-5,3e-4), pct_start=0.05)
+
+accuracy(*learn.TTA())
+
+learn = cnn_learner(data, models.resnet50, metrics=accuracy)
+learn.fit_one_cycle(1)
+
+learn.unfreeze()
+learn.fit_one_cycle(6, slice(1e-5,3e-4), pct_start=0.05)
+
+accuracy(*learn.TTA())
