b680ebbe2c03a49cfcdb1b37877a863f16e42649
probably-not-porter <pelibby16@earlham.edu>
2020-02-18 13:49:26 -0500

    resnet18 example
    Former-commit-id: d8b79b2cc6091473dee5b0126865506dfdf63dbf

--- fast_test.py
+++ fast_test.py
@@ -2,24 +2,10 @@
 # https://github.com/fastai/fastai/blob/master/examples/dogs_cats.ipynb
 
 from fastai.vision import *
+import fastai
 
-path = untar_data(URLs.DOGS)
-
-data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-data.show_batch(rows=4)
-
-learn = cnn_learner(data, models.resnet34, metrics=accuracy)
-learn.fit_one_cycle(1)
-
-learn.unfreeze()
-learn.fit_one_cycle(6, slice(1e-5,3e-4), pct_start=0.05)
-
-accuracy(*learn.TTA())
-
-learn = cnn_learner(data, models.resnet50, metrics=accuracy)
-learn.fit_one_cycle(1)
-
-learn.unfreeze()
-learn.fit_one_cycle(6, slice(1e-5,3e-4), pct_start=0.05)
-
-accuracy(*learn.TTA())
+path = untar_data(URLs.PETS)
+print(path)
+data = ImageDataBunch.from_folder(path, train="images", valid="annotations", size=26)
+learn = cnn_learner(data, models.resnet18, metrics=accuracy)
+learn.fit(1)
