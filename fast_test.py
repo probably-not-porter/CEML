0a6c80b6f27eeaf03f63d5beac8dd271a110c92d
probably-not-porter <pelibby16@earlham.edu>
2020-02-20 19:15:26 -0500

    change batch size
    Former-commit-id: bec59d2dcf42530aa4894013c21dd08019c08a76

--- fast_test.py
+++ fast_test.py
@@ -6,6 +6,6 @@ from fastai.vision import *
 
 
 path = untar_data(URLs.PLANET_SAMPLE)
-data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+data = ImageDataBunch.from_folder(path, bs=32, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
 learn = cnn_learner(data, models.resnet34, metrics=accuracy)
 learn.fit_one_cycle(1)
