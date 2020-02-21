7de82ae9fb35fb711f231a17f5a8ef6cf2ce24d1
probably-not-porter <pelibby16@earlham.edu>
2020-02-20 19:18:40 -0500

    try dogs urls
    Former-commit-id: 0599924b5c329c5260045748b42d4b1fd8ded21d

--- fast_test.py
+++ fast_test.py
@@ -5,7 +5,7 @@ from fastai.vision import *
 
 
 
-path = untar_data(URLs.PLANET_SAMPLE)
+path = untar_data(URLs.DOGS)
 data = ImageDataBunch.from_folder(path, bs=32, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
 learn = cnn_learner(data, models.resnet34, metrics=accuracy)
 learn.fit_one_cycle(1)
