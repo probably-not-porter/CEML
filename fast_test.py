17e7464721a3f106c598236b328229ed0d70e223
probably-not-porter <pelibby16@earlham.edu>
2020-02-20 22:31:56 -0500

    add notebook and test files
    Former-commit-id: 5c8c0f676f96514bed2d7726ee0c6f0ef0b0e1b3
    Former-commit-id: cd21f062322ced82b3cf33dd867de007c8c6b9ae

--- fast_test.py
+++ fast_test.py
@@ -2,13 +2,30 @@
 # https://github.com/fastai/fastai/blob/master/examples/dogs_cats.ipynb
 
 from fastai.vision import *
+from fastai.metrics import accuracy
 
+path = "./dogs-cats"
+size = 224
+bs = 16
 
+import os
+labels = os.listdir("dogs-cats")
+print("No. of labels: {}".format(len(labels)))
+print("-----------------")
 
-path = untar_data(URLs.DOGS)
-print(path)
-data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-data.show_batch(rows=4)
-data = ImageDataBunch.from_folder(path, bs=32, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-learn = cnn_learner(data, models.resnet34, metrics=accuracy)
-learn.fit_one_cycle(1)
+for label in labels:
+    print("{}, {} files".format(label, len(os.listdir("dogs-cats/"+label))))
+
+data = ImageDataBunch.from_folder(path, 
+    ds_tfms=get_transforms(do_flip=True, flip_vert=True),
+    valid_pct=0.2, 
+    size=size, 
+    bs=bs)
+
+data.normalize(imagenet_stats)
+
+learner = cnn_learner(data, models.resnet18, metrics=[accuracy])
+learner.fit_one_cycle(3,max_lr=1e-2)
+
+interp = ClassificationInterpretation.from_learner(learner)
+interp.plot_confusion_matrix(figsize=(10,10))
