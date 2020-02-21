2142427fd85b793d7a9853e799779c57f3e3374d
probably-not-porter <pelibby16@earlham.edu>
2020-02-20 20:36:59 -0500

    try again
    Former-commit-id: 9ba40f76eb9c334d4e44cac6ad992de7cb7c1062

--- fast_test.py
+++ fast_test.py
@@ -6,6 +6,9 @@ from fastai.vision import *
 
 
 path = untar_data(URLs.DOGS)
+print(path)
+data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+data.show_batch(rows=4)
 data = ImageDataBunch.from_folder(path, bs=32, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
 learn = cnn_learner(data, models.resnet34, metrics=accuracy)
 learn.fit_one_cycle(1)
