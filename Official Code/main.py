6af07dbb2346b432c8f64f20a68439a3f7ca27c3
probably-not-porter <pelibby16@earlham.edu>
2020-04-09 16:00:17 -0400

    fix some bugs, run some tests
    Former-commit-id: 7b48bf845463f980a9ede6e0e5b4067fae019812
    Former-commit-id: 9e201885402e95c7c7c0bd43af8a1a6bc4362ca3
    Former-commit-id: 568c913f86ba829c1cc3f57d19ec492086cfbbba

--- Official Code/main.py
+++ Official Code/main.py
@@ -11,12 +11,4 @@ import os
 from dataglob import DataGlob # Data structure
 
 
-# testing
-glob1 = DataGlob(os.getcwd() + "/training/plant-id",os.getcwd() +"/out")
-glob1.print_configuration()
-glob1.set_configuration("caney_auto", True)
-#glob1.set_configuration("caney_wide", True)
-glob1.set_configuration("original", True)
-glob1.print_configuration()
-glob1.prepare_database()
-glob1.create_databunch()
+
