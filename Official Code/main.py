50eaa706c861da2b84ff056dd3135c1ffcd52df0
probably-not-porter <pelibby16@earlham.edu>
2020-04-12 17:08:30 -0400

    test notebooks
    Former-commit-id: 466fafd960014371f20b54a9f5c11c7f83a4d008
    Former-commit-id: 11904152870a51d8fb8d0baff560c35c5da058b5
    Former-commit-id: 26c85769a0c9475ceb18a5d9e35259e11bde8711

--- Official Code/main.py
+++ Official Code/main.py
@@ -10,5 +10,17 @@ import os
 # import custom code
 from dataglob import DataGlob # Data structure
 
+url = "/training/chest-xray-small"
 
 
+glob1 = DataGlob(os.getcwd() + url,os.getcwd())
+glob1._overwrite = True
+
+# adjust some knobs
+glob1.set_configuration("original", True)
+glob1.set_configuration("sobel_y", True)
+glob1.set_configuration("prewitt", True)
+
+print('CANEY_TIGHT AND ORIGINAL')
+glob1.prepare_database()
+#glob1.create_databunch()
